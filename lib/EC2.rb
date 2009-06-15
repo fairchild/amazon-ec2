af2515ac1371079e7fadcb27cc5b080882a2135b
fairchild <fairchild@stimble.net>
2009-06-15 12:16:48 -0700

    Support eucalyptus clouds. If ENV['EC2_URL'] is set, add this to the list of valid hosts.

--- lib/EC2.rb
+++ lib/EC2.rb
@@ -25,7 +25,7 @@ module EC2
   #
   if ENV['EC2_URL']
     EC2_URL = ENV['EC2_URL']
-    VALID_HOSTS = ['https://ec2.amazonaws.com', 'https://us-east-1.ec2.amazonaws.com', 'https://eu-west-1.ec2.amazonaws.com']
+    VALID_HOSTS = ['https://ec2.amazonaws.com', 'https://us-east-1.ec2.amazonaws.com', 'https://eu-west-1.ec2.amazonaws.com', ENV['EC2_URL']].compact
     raise ArgumentError, "Invalid EC2_URL environment variable : #{EC2_URL}" unless VALID_HOSTS.include?(EC2_URL)
     DEFAULT_HOST = URI.parse(EC2_URL).host
   else
